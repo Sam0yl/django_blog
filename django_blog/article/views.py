63dd2924652544a081a5e1cd3a79b6163fd07335
Evgeny Samoylov <samoyl@gmail.com>
2023-08-04 16:33:59 +0200

    add article template

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'article/index.html', context={
+        'article': 'article'})
