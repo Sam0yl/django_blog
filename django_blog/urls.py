fa1bce5cbbd8613199940acef5826d12204a1fda
Evgeny Samoylov <samoyl@gmail.com>
2023-08-04 18:54:34 +0200

    add base template

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -20,7 +20,7 @@ from django_blog import views
 
 urlpatterns = [
     path('', views.index),
-    path('articles/', include('django_blog.article.urls')),
+    path('article/', include('django_blog.article.urls')),
     path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
