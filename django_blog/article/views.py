f7130a35b6dda31faac3541e8400b72cbd4ff8fc
Evgeny Samoylov <samoyl@gmail.com>
2023-08-07 16:11:44 +0200

    add articles view

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.views import View
-
+from django_blog.article.models import Article
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'article/index.html', context={'article': 'article'})
+        articles = Article.objects.all()[0:15]
+        return render(request, 'articles/index.html', context={'articles': articles})
+
 
 def index(request, tags, article_id):
     return render(request, 'article/index.html', context={
