aa42bc4e18d4c33b8d181ce82d6aaa22fa51cae3
Evgeny Samoylov <samoyl@gmail.com>
2023-08-08 10:57:26 +0200

    add article view

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from django_blog.article import views
+from django_blog.article.views import IndexView, ArticleView
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<str:tags>/<int:article_id>/', views.index, name='article')
+    path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view(), name='article')
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
 from django.views import View
 from django_blog.article.models import Article
 
@@ -11,7 +10,13 @@ class IndexView(View):
         return render(request, 'articles/index.html', context={'articles': articles})
 
 
-def index(request, tags, article_id):
-    return render(request, 'article/index.html', context={
-        'tags': tags,
-        'article_id': article_id})
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={'article': article,})
+
+
+# def index(request, tags, article_id):
+#     return render(request, 'article/index.html', context={
+#         'tags': tags,
+#         'article_id': article_id})
