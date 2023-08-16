30f8876fe491e65bc44dce9007caa976515d67ec
Evgeny Samoylov <samoyl@gmail.com>
2023-08-16 12:26:13 +0200

    add form for update articles

--- django_blog/article/forms.py
+++ django_blog/article/forms.py
@@ -1,10 +1,23 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Article
 
 
-class ArticleForm(ModelForm):
+class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['name', 'body']
+    
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) > 50:
+            raise forms.ValidationError("Name must be at most 50 characters long.")
+        return name
+    
+    def clean_body(self):
+        body = self.cleaned_data['body']
+        if len(body) > 1000:
+            raise forms.ValidationError("Body must be at most 1000 characters long.")
+        return body
+
         
 
--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from django_blog.article.views import IndexView, ArticleFormCreateView, ArticleView
+from django_blog.article.views import IndexView, ArticleFormCreateView, ArticleFormEditView, ArticleView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='articles'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', ArticleView.as_view(), name='article'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
